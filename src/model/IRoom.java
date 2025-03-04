692480785f0b6d75463acacd8d76d658f803efdd
ernest nnodu <ernest.nnodu@outlook.com>
2025-03-04 11:11:28 +0000

    Create room model

--- src/model/IRoom.java
+++ src/model/IRoom.java
@@ -2,8 +2,8 @@ package model;
 
 public interface IRoom {
 
-    public String getRoomNumber();
-    public Double getRoomPrice();
-    public RoomType getRoomType();
-    public boolean isFree();
+    String getRoomNumber();
+    Double getRoomPrice();
+    RoomType getRoomType();
+    boolean isFree();
 }
